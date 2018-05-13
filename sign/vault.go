47ade22ecc035e7c94b9e2ffbaa4a5d085b82e20
Christian Svensson <blue@cmd.nu>
2018-05-13 16:45:48 +0200

    create vault tokens

--- sign/vault.go
+++ sign/vault.go
@@ -1,6 +1,7 @@
 package sign
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -15,6 +16,7 @@ var (
 	vaultSshMount = flag.String("vault_ssh_mount", "ssh", "Mount point for the SSH signer in Vault")
 	vaultTtl      = flag.String("vault_ttl", "20h", "Validity duration of signed artificats from Vault")
 	vaultRenew    = flag.String("vault_renew", "24h", "How often to renew the token")
+	vaultGroupMap = flag.String("vault_group_map", "{}", "JSON group map from LDAP group DN to policy")
 )
 
 type Auditor interface {
@@ -24,6 +26,7 @@ type Auditor interface {
 type signer struct {
 	a Auditor
 	v *vault.Client
+	gmap map[string]string
 }
 
 func (s *signer) renewer() {
@@ -72,6 +75,33 @@ func (s *signer) Sign(r *pb.UserCredentialRequest, u *pb.VerifiedUser) (*pb.Cred
 		artifacts = append(artifacts, "SSH certificate")
 	}
 
+	if r.VaultTokenRequest != nil {
+		policies := make([]string, 0)
+		for _, group := range u.Group {
+			p, ok := s.gmap[group]
+			if !ok {
+				continue
+			}
+			policies = append(policies, p)
+		}
+		log.Printf("Creating Vault token for %s with policies %v", u.Username, policies)
+
+		tcr := &vault.TokenCreateRequest{
+			Metadata: map[string]string{"username": u.Username},
+			TTL: *vaultTtl,
+			Policies: policies,
+		}
+		sk, err := s.v.Auth().Token().CreateWithRole(tcr, "user")
+		if err != nil {
+			log.Printf("failed to create Vault token: %v", err)
+			return nil, fmt.Errorf("failed to create Vault token")
+		}
+		res.VaultToken = &pb.VaultToken{
+			Token: sk.Auth.ClientToken,
+		}
+		artifacts = append(artifacts, "Vault token")
+	}
+
 	s.a.Log(fmt.Sprintf("signed %s for %s", strings.Join(artifacts, ", "), u.Username))
 	return res, nil
 }
@@ -84,6 +114,15 @@ func New(a Auditor) *signer {
 	s := &signer{
 		a: a,
 		v: v,
+	}
+	s.gmap = make(map[string]string)
+	var j interface{}
+	err = json.Unmarshal([]byte(*vaultGroupMap), &j)
+	if err != nil {
+		log.Fatalf("could not parse Vault group map: %v", err)
+	}
+	for k, v := range j.(map[string]interface{}) {
+		s.gmap[k] = v.(string)
 	}
 	go s.renewer()
 	return s
